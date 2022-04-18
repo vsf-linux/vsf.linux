ade79338da2b694fc6c731138cbd65f058824549
vsfos <vsfos@foxmail.com>
2022-04-19 01:20:40 +0800

    [wamr] aot working

--- application/3rd-party/wamr/port/wamr_port.c
+++ application/3rd-party/wamr/port/wamr_port.c
@@ -1,4 +1,6 @@
 #ifdef BH_PLATFORM_WINDOWS
+#include <Windows.h>
+
 int os_thread_env_init()
 {
     return 0;
@@ -6,5 +8,16 @@ int os_thread_env_init()
 
 void os_thread_env_destroy()
 {
+}
+
+void os_dcache_flush(void)
+{
+}
+
+unsigned os_getpagesize()
+{
+    SYSTEM_INFO sys_info;
+    GetNativeSystemInfo(&sys_info);
+    return (unsigned)sys_info.dwPageSize;
 }
 #endif
