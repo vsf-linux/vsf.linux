658fa7fa568e65afb469f88b6b7a685351653c41
vsfos <vsfos@foxmail.com>
2022-06-03 19:32:13 +0800

    [application/openocd] update config

--- application/3rd-party/openocd/port/jimtcl/jim-config.h
+++ application/3rd-party/openocd/port/jimtcl/jim-config.h
@@ -1,3 +1,9 @@
-/* Public autoconf settings */
-#define JIM_VERSION         81
-#define NO_ENVIRON_EXTERN   1
+#ifndef _JIM_CONFIG_H
+#define _JIM_CONFIG_H
+#define HAVE_LONG_LONG 1
+/* #undef JIM_UTF8 */
+#define JIM_VERSION 81
+#define SIZEOF_INT 4
+
+#define NO_ENVIRON_EXTERN
+#endif
