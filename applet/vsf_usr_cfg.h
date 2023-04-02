4d925ea268f9ba71873b0f5458ee75830aa045c5
vsfos <vsfos@foxmail.com>
2023-04-02 22:42:53 +0800

    [applet] update [vsf] sync

--- applet/vsf_usr_cfg.h
+++ applet/vsf_usr_cfg.h
@@ -6,40 +6,37 @@
 /*============================ INCLUDES ======================================*/
 /*============================ MACROS ========================================*/
 
-#define __CPU_ARM__
 #define __VSF_APPLET__
+
+// arch
+
+// arm
+
+#define __CPU_ARM__
 // no program_start from CMSIS Core
 #define __PROGRAM_START                                 _start
 
+// linux
+
+//#define __VSF64__
+//#define __CPU_GENERIC__
+//#define __LINUX__
+
 // components
+
+#define VSF_USE_TRACE                                   ENABLED
+#define VSF_USE_FS                                      ENABLED
 #define VSF_USE_SIMPLE_STREAM                           ENABLED
+#define VSF_USE_USB_HOST                                ENABLED
 
 // linux
+
 #define VSF_USE_LINUX                                   ENABLED
 #   define VSF_LINUX_LIBC_USE_ENVIRON                   ENABLED
 #   define VSF_LINUX_USE_SIMPLE_LIBC                    ENABLED
+#   define VSF_LINUX_USE_LIBUSB                         VSF_USE_USB_HOST
+#   define VSF_LINUX_USE_SOCKET                         ENABLED
 #   define VSF_LINUX_USE_APPLET                         ENABLED
-#       define VSF_LINUX_APPLET_USE_LIBC                ENABLED
-#       define VSF_LINUX_APPLET_USE_UNISTD              ENABLED
-#       define VSF_LINUX_APPLET_USE_PTHREAD             ENABLED
-#       define VSF_LINUX_APPLET_USE_SEMAPHORE           ENABLED
-#       define VSF_LINUX_APPLET_USE_POLL                ENABLED
-#       define VSF_LINUX_APPLET_USE_NETDB               ENABLED
-#       define VSF_LINUX_APPLET_USE_LIBGEN              ENABLED
-#       define VSF_LINUX_APPLET_USE_IFADDRS             ENABLED
-#       define VSF_LINUX_APPLET_USE_LIBGETOPT           ENABLED
-#       define VSF_LINUX_APPLET_USE_FCNTL               ENABLED
-#       define VSF_LINUX_APPLET_USE_DIRENT              ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_EPOLL           ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_EVENTFD         ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_RANDOM          ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_SELECT          ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_SHM             ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_STAT            ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_TIME            ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_UTSNAME         ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_WAIT            ENABLED
-#       define VSF_LINUX_APPLET_USE_SYS_SOCKET          ENABLED
 
 // APP configuration
 
