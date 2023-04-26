c059669fa6595da7d4982db1659b517b4372e737
vsfos <vsfos@foxmail.com>
2023-04-26 13:54:32 +0800

    [application/tcc] update tccdefs_.h

--- application/3rd-party/tcc/port/tccdefs_.h
+++ application/3rd-party/tcc/port/tccdefs_.h
@@ -1,13 +1,16 @@
+"\
+#define __VSF__\n\
+#define __VSF_APPLET__\n\
+#define __CPU_X64__\n\
+#define __WIN__\n\
+#include <tccdefs.h>\n\
+"
+
 #ifdef __VSF_APPLET__
+// IMPORT: undef _WIN32 and _WIN64 after tccdefs.h
 "\
 #undef _WIN32\n\
 #undef _WIN64\n\
+#include <vsf_cfg.h>\n\
 "
 #endif
-"\
-#define __VSF__\n\
-#define __CPU_X64__\n\
-#define __WIN__\n\
-#define __VSF_APPLET__\n\
-#include <tccdefs.h>\n\
-"
