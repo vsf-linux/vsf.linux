3b4132663a7422c98314b22382b1f40b8a1baf18
vsfos <vsfos@foxmail.com>
2024-01-18 16:08:27 +0800

    [application/busybox] update porting

--- application/3rd-party/busybox/port/vsf_porting/libbb_dynlib.c
+++ application/3rd-party/busybox/port/vsf_porting/libbb_dynlib.c
@@ -3,7 +3,15 @@
 #include <syslog.h>
 #include "libbb.h"
 
-int vsf_busybox_dynlib_idx = -1;
+#define define_vsf_busybox_mod(__mod_name, __mod_size, __mod_idx, __mod_init)   \
+        const vsf_linux_dynlib_mod_t vsf_busybox_mod_name(__mod_name) = {       \
+            .lib_idx            = NULL,                                         \
+            .mod_idx            = (__mod_idx),                                  \
+            .module_num         = 1,                                            \
+            .bss_size           = (__mod_size),                                 \
+            .mod_size           = (__mod_size),                                 \
+            .init               = (__mod_init),                                 \
+        };
 
 static void __busybox_libbb_init(void *ctx)
 {
@@ -13,6 +21,8 @@ static void __busybox_libbb_init(void *ctx)
     logmode = LOGMODE_STDIO;
     msg_eol = "\n";
 
+    // init
+    mod_ctx->init.message.log_fd = -1;
     // verror_msg.c
     mod_ctx->verror_msg.syslog_level = LOG_ERR;
 }
--- application/3rd-party/busybox/port/vsf_porting/libbb_dynlib.h
+++ application/3rd-party/busybox/port/vsf_porting/libbb_dynlib.h
@@ -5,20 +5,8 @@
 #define vsf_busybox_mod_name(__mod_name)      __vsf_busybox_mod_name(__mod_name)
 #define declare_vsf_busybox_mod(__mod_name)                                     \
         extern const vsf_linux_dynlib_mod_t vsf_busybox_mod_name(__mod_name);
-#define define_vsf_busybox_mod(__mod_name, __mod_size, __mod_idx, __mod_init)   \
-        const vsf_linux_dynlib_mod_t vsf_busybox_mod_name(__mod_name) = {       \
-            .lib_idx            = &vsf_busybox_dynlib_idx,                      \
-            .mod_idx            = (__mod_idx),                                  \
-            .module_num         = VSF_BUSYBOX_MOD_NUM,                          \
-            .bss_size           = VSF_BUSYBOX_BSS_SIZE,                         \
-            .mod_size           = (__mod_size),                                 \
-            .init               = (__mod_init),                                 \
-        };
-#define VSF_BUSYBOX_BSS_SIZE      32768
 enum {
     VSF_BUSYBOX_MOD_LIBBB = 0,
-    VSF_BUSYBOX_MOD_NUM,
 };
-extern int vsf_busybox_dynlib_idx;
 
 #endif      // __BUSYBOX_PORT_VSF_H__
